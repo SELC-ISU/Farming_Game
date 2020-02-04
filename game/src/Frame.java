42bc07521eab40930e777f3507d6ef843a926ec6
mjgwin <micahjgwin@gmail.com>
2020-02-04 10:11:39 -0600

    Fixed spelling

--- game/src/Frame.java
+++ game/src/Frame.java
@@ -5,7 +5,7 @@ public class Frame extends JFrame{
 
     public Frame(){
         setSize(new Dimension(1280,720)); //Creates size value for window
-        setTitle("Farming Gane");
+        setTitle("Farming Game");
         setDefaultCloseOperation(Frame.EXIT_ON_CLOSE); //After the red x is hit, window will close
         setLocationRelativeTo(null); //Puts window in middle of screen
         setVisible(true);
