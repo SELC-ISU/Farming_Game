19fa9030eb510bddc97e10e6a0e61986668e5f4b
mjgwin <micahjgwin@gmail.com>
2020-02-04 10:10:02 -0600

    Added frame class to make game window

---
+++ game/src/Frame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame{
+
+    public Frame(){
+        setSize(new Dimension(1280,720)); //Creates size value for window
+        setTitle("Farming Gane");
+        setDefaultCloseOperation(Frame.EXIT_ON_CLOSE); //After the red x is hit, window will close
+        setLocationRelativeTo(null); //Puts window in middle of screen
+        setVisible(true);
+    }
+
+
+}
--- game/src/MainGame.java
+++ game/src/MainGame.java
@@ -2,7 +2,9 @@
 public class MainGame{
 
     public static void main(String[] args){
-        System.out.println("Hello");
+
+        System.out.println("Starting game...");
+        Frame frame = new Frame(); //Creates new frame object causing window to pop up
     }
 
 }
