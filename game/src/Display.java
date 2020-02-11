8fdb02626cfd6b862072545bfc2cf88fcecd3526
Kasper1019 <Bell@ 2016>
2020-02-11 10:04:16 -0600

    class to move rectangle large distances

--- game/src/Display.java
+++ game/src/Display.java
@@ -5,7 +5,12 @@ public class Display extends JPanel {
 
     int x, y, width, height;
 
-
+    public void setRectangleDimensions(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 
     public Display(Input keyInput){
         addKeyListener(keyInput);
