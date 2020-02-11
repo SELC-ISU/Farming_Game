f3114277e048387cae1b103896d6f2fb94cfdb67
Kasper1019 <Bell@ 2016>
2020-02-11 10:09:46 -0600

    class to initialize rectangle

--- game/src/Display.java
+++ game/src/Display.java
@@ -12,6 +12,13 @@ public class Display extends JPanel {
         this.height = height;
     }
 
+    public void initializeRectangleDimensions(){
+        this.x = 100;
+        this.y = 100;
+        this.width = 50;
+        this.height = 50;
+    }
+
     public Display(Input keyInput){
         addKeyListener(keyInput);
         setFocusable(true);
