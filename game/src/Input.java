c756811309f115cae298c94cad9717e14bc2a058
Kasper1019 <Bell@ 2016>
2020-02-11 09:18:52 -0600

    added key char

--- game/src/Input.java
+++ game/src/Input.java
@@ -2,9 +2,12 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Input implements KeyListener {
+
+    public char key = '!';
+
     @Override
     public void keyTyped(KeyEvent e) {
-        
+        key = e.getKeyChar();
     }
 
     @Override
