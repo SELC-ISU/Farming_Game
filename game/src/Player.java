342cd680df3e4bced1be312c4135e7220d31c8c8
Kasper1019 <Bell@ 2016>
2020-03-29 20:05:29 -0500

    adding x and y postitions

--- game/src/Player.java
+++ game/src/Player.java
@@ -1,4 +1,21 @@
 
 public class Player {
 
+	private int x;
+	private int y;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	
 }
